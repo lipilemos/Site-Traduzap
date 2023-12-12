d846a5d03f068ed6e88b997e0cd8b55dc89d611e
felipe.lemos <felipe.lemos@mutant.com.br>
2023-12-12 12:16:32 -0300

    url pord

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,12 +1,12 @@
 
 //prod
-//export const api = "https://api.traduzapp.com.br:3000/api";
-//export const uploads = "https://api.traduzapp.com.br:3000/uploads";
+export const api = "https://api.traduzapp.com.br:3000/api";
+export const uploads = "https://api.traduzapp.com.br:3000/uploads";
 export const MERCADOPAGO_PUBLIC_KEY = "APP_USR-0513651a-c5f6-4fa1-8ffa-6e7726b7a4c2";
 export const MERCADOPAGO_TOKEN ="APP_USR-8692405970692299-102715-51bd6b29d0bdb00b6b0e982b2d325b65-102783479";
 //dev
-export const api = "http://localhost:5000/api"
-export const uploads = "http://localhost:5000/uploads";
+//export const api = "http://localhost:5000/api"
+//export const uploads = "http://localhost:5000/uploads";
 //export const MERCADOPAGO_PUBLIC_KEY = "TEST-6ae6319a-a4d1-4f71-8d65-21a3ebcf8278";
 //export const MERCADOPAGO_TOKEN ="TEST-8692405970692299-102715-2fa856e37823b179a4a37bb727eb794d-102783479";
 
